2efeb88f82112f80cebc7bf543c24682d13be6b9
Oh-jj <fvjung01@gmail.com>
2025-05-11 20:41:31 +0900

    FEAT 유닛테스트 한정 커버리지

--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,9 @@ export default {
     globals: true,
     environment: 'node',
     setupFiles: [],
+    coverage: {
+      reporter: ['text', 'lcov'], // 텍스트/HTML 리포트 출력 (선택사항)
+      include: ['src/services/**'], // ✅ 이 경로만 커버리지 포함
+    },
   },
 };
